fd74f1c67609b7b42acbb429b76cc60dcbe8b4ec
Louis Travaux <louis.trvx@gmail.com>
2022-02-05 14:55:54 +0100

    continuité accueil

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -1,6 +1,8 @@
 import { emitDistinctChangesOnlyDefaultValue } from '@angular/compiler';
+import { trimTrailingNulls } from '@angular/compiler/src/render3/view/util';
 import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { max } from 'rxjs';
 
 @Component({
   selector: 'accueil',
@@ -15,24 +17,28 @@ export class Accueil implements OnInit, AfterViewInit {
 
   ngAfterViewInit() {
     window.addEventListener('scroll', function() {
-      var currentPosition = window.scrollY || document.documentElement.scrollTop || document.body.scrollTop || 0;
-      var maxPosition = Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight ); // max scroll position (cross-plateform)
+      var currentPosition = Math.max(window.scrollY || document.documentElement.scrollTop || document.body.scrollTop || 0);
+      const maxPosition = Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight ); // max scroll position (cross-plateform)
 
-      var avancementScroll = (currentPosition/maxPosition);
+      var perCent_currentScroll = (currentPosition/maxPosition)*100; // emplacement scroll actuel sur page (%)
+      var tailleSection = maxPosition/4; // 4: hauteur du site (4 fois 100vh)
+      var facteurOpacity = (currentPosition%tailleSection)/tailleSection;
 
-      var tailleSection = maxPosition/4 ;
-
-      if(currentPosition>0 && currentPosition<tailleSection+100) {
-        this.document.getElementById("s-1")!.style.opacity = String(Math.max(1-3*avancementScroll, 0.1));
-        this.document.getElementById("s-s-1")!.style.opacity = String(Math.min(3*(avancementScroll), 1));
-        this.document.getElementById("s-s-2")!.style.opacity = String(Math.max(Math.min(3*(avancementScroll-.25), 0.7), 0.1));
-      }else if(currentPosition>tailleSection+200 && currentPosition<(2*(tailleSection)+200)) {
-        this.document.getElementById("s-s-1")!.style.opacity = String(Math.max(.8-avancementScroll, 0.1));
-        this.document.getElementById("s-s-2")!.style.opacity = String(Math.min(3*(avancementScroll-.25), 1));
-      } else if (currentPosition>(2*tailleSection-100) && currentPosition<(3*(tailleSection))) {
-        this.document.getElementById("s-s-2")!.style.opacity = String(Math.max(.8-avancementScroll, 0.1));
-        this.document.getElementById("s-3")!.style.opacity = String((Math.min(10*(avancementScroll-.5)+.2, 1)));
-        console.log(this.document.getElementById("s-3")?.style.opacity);
+      if(currentPosition>0 && currentPosition<tailleSection) 
+      {
+        this.document.getElementById("s-1")!.style.opacity = String(Math.max(1-facteurOpacity, 0.2));
+        this.document.getElementById("s-s-1")!.style.opacity = String(Math.min(facteurOpacity, 1));
+      }
+      else if(currentPosition>tailleSection && currentPosition<2*tailleSection) 
+      {
+        this.document.getElementById("s-s-1")!.style.opacity = String(Math.max(1-facteurOpacity, 0.2));
+        this.document.getElementById("s-s-2")!.style.opacity = String(Math.min(facteurOpacity, 1));
+        console.log(tailleSection);
+      }
+      else if(currentPosition>2*tailleSection && currentPosition<3*tailleSection)
+      {
+        this.document.getElementById("s-s-2")!.style.opacity = String(Math.max(1-facteurOpacity, 0.2));
+        this.document.getElementById("s-3")!.style.opacity = String(Math.min(facteurOpacity, 1));
       }
     });
   }
