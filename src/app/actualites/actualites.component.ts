98a80f3c29af300f11f0f0e7890feb8a017556e8
Louis Travaux <louis.trvx@gmail.com>
2022-02-07 20:12:06 +0100

    page actu: structure --> OK

--- src/app/actualites/actualites.component.ts
+++ src/app/actualites/actualites.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GalerieComponent } from '../galerie/galerie.component';
 
 @Component({
   selector: 'app-actualites',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./actualites.component.scss']
 })
 export class ActualitesComponent implements OnInit {
+  images = images;
 
   constructor() { }
 
@@ -13,3 +15,37 @@ export class ActualitesComponent implements OnInit {
   }
 
 }
+
+export interface image {
+  path: string;
+  id: number;
+  desc: string;
+  date: string;
+}
+
+export const images = [
+  { 
+    path: "volant_lmp3.png", 
+    id: 11, 
+    desc: "Essais LMP3 (jour 2)", 
+    date: "21 novembre 2020"
+  },
+  { 
+    path: "victoire_fnac.jpeg",
+    id: 12, 
+    desc: "Victoire Fnac Thronos Challenge", 
+    date: "31 juillet 2021"
+  },
+  { 
+    path: "maxime_lemans.jpg",
+    id: 13, 
+    desc: "Le Mans Virtual : 3ème relais", 
+    date: "15-16 janvier 2022"
+  },
+  { 
+    path: "le_mans_virtual.webp",
+    id: 14, 
+    desc: "Le Mans Virtual : P12 avec le Duqueine Team", 
+    date: "15-16 janvier 2022"
+  }
+]
