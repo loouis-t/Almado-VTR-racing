e229f8f969fb1e7451344dc100081e054fc65b38
Louis Travaux <louis.trvx@gmail.com>
2022-02-14 20:29:55 +0100

    tri images /galerie ordre chronologique + reglage menu mobile

--- src/app/galerie/galerie.component.ts
+++ src/app/galerie/galerie.component.ts
@@ -15,7 +15,6 @@ export class GalerieComponent implements OnInit {
 
 }
 
-
 export interface image {
   path: string;
   id: number;
@@ -25,95 +24,77 @@ export interface image {
 
 export const images = [
   { 
+    path: "combi_devant_ds3.jpeg",
+    desc: "Karting de muret",
+    date: "25 octobre 2020"
+  },
+  { 
+    path: "circuit_karting.jpeg",
+    desc: "Karting de muret",
+    date: "25 octobre 2020"
+  },
+  { 
     path: "devant_karting.jpeg",
-    id: 0, 
     desc: "Karting de muret", 
     date: "25 octobre 2020"
   },
   { 
-    path: "maxime_casque.png",
-    id: 1, 
-    desc: "Essais LMP3 (jour 2)", 
-    date: "21 novembre 2020" 
-  },
-  { 
-    path: "circuit_karting.jpeg",
-    id: 2,
-    desc: "Karting de muret",
-    date: "25 octobre 2020"
-  },
-  { 
-    path: "lmp3.jpeg", 
-    id: 3,
-    desc: "Essais LMP3 (jour 1)", 
-    date: "19 novembre 2020"
-  },
-  { 
-    path: "dans_karting.jpeg", id: 4, desc: "Karting de muret", date: "25 octobre 2020"
-  },
-  { 
-    path: "devant_f4_lmp3.jpeg", 
-    id: 5, 
-    desc: "Essais LMP3 (jour 1)",
-    date: "19 novembre 2020"
-  },
-  { 
-    path: "combi_devant_ds3.jpeg",
-    id: 6, 
-    desc: "Karting de muret",
+    path: "dans_karting.jpeg",
+    desc: "Karting de muret", 
     date: "25 octobre 2020"
   },
   { 
     path: "nom_sur_lmp3.png",
-    id: 7, 
     desc: "Essais LMP3 (jour 1)", 
     date: "19 novembre 2020"
   },
   { 
-    path: "dans_simulateur.jpeg",
-    id: 8, 
-    desc: "Essai TGT II, partenariat avec Thrustmaster", 
-    date: "19 juin 2021"
-  },
-  { 
-    path: "devant_lmp3.jpeg", 
-    id: 9, 
-    desc: "Essais LMP3 (jour 1)", 
-    date: "19 novembre 2020" 
-  },
-  { 
-    path: "setup_simulateur.jpeg", 
-    id: 10, 
-    desc: "Setup: Thrustmaster TGT II, NLR F-GT, ...",
-    date: "21 juin 2021" 
-  },
-  { 
-    path: "volant_lmp3.png", 
-    id: 11, 
+    path: "volant_lmp3.png",
     desc: "Essais LMP3 (jour 2)", 
     date: "21 novembre 2020"
   },
   { 
+    path: "lmp3.jpeg",
+    desc: "Essais LMP3 (jour 1)", 
+    date: "19 novembre 2020"
+  },
+  { 
+    path: "devant_lmp3.jpeg",
+    desc: "Essais LMP3 (jour 1)", 
+    date: "19 novembre 2020" 
+  },
+  { 
+    path: "devant_f4_lmp3.jpeg",
+    desc: "Essais LMP3 (jour 1)",
+    date: "19 novembre 2020"
+  },
+  { 
+    path: "maxime_casque.png",
+    desc: "Essais LMP3 (jour 2)", 
+    date: "21 novembre 2020" 
+  },
+  { 
+    path: "dans_simulateur.jpeg",
+    desc: "Essai TGT II, partenariat avec Thrustmaster", 
+    date: "19 juin 2021"
+  },
+  { 
+    path: "setup_simulateur.jpeg",
+    desc: "Setup: Thrustmaster TGT II, NLR F-GT, ...",
+    date: "21 juin 2021" 
+  },
+  { 
     path: "victoire_fnac.jpeg",
-    id: 12, 
     desc: "Victoire Fnac Thronos Challenge", 
     date: "31 juillet 2021"
   },
   { 
     path: "maxime_lemans.jpeg",
-    id: 13, 
     desc: "Le Mans Virtual : 3ème relais", 
     date: "15-16 janvier 2022"
   },
   { 
-    path: "le_mans_virtual.webp",
-    id: 14, 
-    desc: "Le Mans Virtual : P12 avec le Duqueine Team", 
-    date: "15-16 janvier 2022"
-  },
-  { 
     path: "Maxime-LMV-2022.jpeg",
-    id: 15, 
     desc: "Le Mans Virtual : équipe PRiMA-Duqueine", 
     date: "15-16 janvier 2022"
   }
